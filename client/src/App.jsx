250669b6c7eb0152aa426a1db943074da9cb8b8c
AmoghDevWorks <fakemail2k05@gmail.com>
2025-08-03 17:03:19 +0530

    router dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
+
+const Structure = () =>{
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
+}
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Structure />,
+    children:[
+      {
+        path:'/',
+        element:<Home />
+      },
+      {
+        path:'/about',
+        element:<About />
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div className='min-h-screen min-w-screen bg-red-500'>
-      dfa
-    </div>
+    <RouterProvider router={appRouter}>
+      <Structure />
+    </RouterProvider>
   )
 }
 
